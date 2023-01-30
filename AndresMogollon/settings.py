4db5c2ac06495768caf112e9e5759619704d766b
andrekasjas <andresmogollob@gmail.com>
2023-01-30 13:49:56 -0500

    a

--- AndresMogollon/settings.py
+++ AndresMogollon/settings.py
@@ -128,12 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     BASE_DIR/ 'static',
 )
-STATIC_URL = '/static/'
+
 if not DEBUG:    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
