68fca22c224134f7d70749ff7a9791509fec4a85
andrekasjas <andresmogollob@gmail.com>
2023-02-02 12:20:36 -0500

    s

--- AndresMogollon/settings.py
+++ AndresMogollon/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'AndresMogollon.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600    
+    )
 }
 
 
